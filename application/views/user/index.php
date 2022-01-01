72ce05b51050c2b941356c83134a2fee54304f25
akbarverari1 <akbar.verary91@gmail.com>
2022-01-01 23:26:22 +0700

    Update Tubes

---
+++ application/views/user/index.php
@@ -0,0 +1,80 @@
+  <section id="banner">
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+          <ol class="carousel-indicators">
+              <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+              <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+              <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+              <div class="carousel-item active">
+                  <img src="https://www.markuptag.com/images/slider-img-1.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                      <h3>Let's Make Life More Meaningful</h3>
+                      <p>Using gadgets without remembering time is a bad act.with us ,it will help you managing time when using gadgets</p>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                  <img src="https://www.markuptag.com/images/slider-img-2.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h3>Let's Make Life More Meaningful</h3>
+                    <p>Using gadgets without remembering time is a bad act.with us ,it will help you managing time when using gadgets</p>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                  <img src="https://www.markuptag.com/images/slider-img-3.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h3>Let's Make Life More Meaningful</h3>
+                    <p>Using gadgets without remembering time is a bad act.with us ,it will help you managing time when using gadgets</p>
+                  </div>
+              </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+          </a>
+      </div>
+  </section>
+  <div class="container shadow mt-5 border">
+    <div class="row">
+        <div class="col-md-4 col-sm-6 bg-light text-center border-end border-bottom py-5 px-3">
+            <i class="bi bi-people text-danger fs-1"></i>
+            <img src="assets/shaungram/pic1.png" class="rounded" alt="pic1">
+            <h4 class="pt-3 mb-3 fw-normal text-uppercase">Set Your Access Restrictions</h4>
+            <p class="text-secondary">You can restrict which apps you can and can not use!</p>
+        </div>
+        <div class="col-md-4 col-sm-6 bg-light text-center border-end border-bottom py-5 px-3">
+            <i class="bi bi-images text-danger fs-1"></i>
+            <img src="assets/shaungram/pic2.png" class="rounded" alt="pic2">
+            <h4 class="pt-4 mb-3 fw-normal text-uppercase">Parental Control</h4>
+            <p class="text-secondary">Monitor your child’s activities with SHAUMGRAM!</p>
+        </div>
+        <div class="col-md-4 col-sm-6 bg-light text-center border-bottom py-5 px-3">
+            <i class="bi bi-shop text-danger fs-1"></i>
+            <img src="assets/shaungram/pic3.png" class="rounded" alt="pic3">
+            <h4 class="pt-3 mb-3 fw-normal text-uppercase">Lockscreen Count</h4>
+            <p class="text-secondary">LoCount how many times you open the Lockscreen!</p>
+        </div>
+        <div class="col-md-4 col-sm-6 bg-light text-center border-end py-5 px-3">
+            <i class="bi bi-pencil-square text-danger fs-1"></i>
+            <img src="assets/shaungram/pic4.png" class="rounded" alt="pic4">
+            <h4 class="pt-3 mb-3 fw-normal text-uppercase">Statistics</h4>
+            <p class="text-secondary">Get static use of your device per week!</p>
+        </div>
+        <div class="col-md-4 col-sm-6 bg-light text-center border-end py-5 px-3">
+            <i class="bi bi-briefcase text-danger fs-1"></i>
+            <img src="assets/shaungram/pic5.png" class="rounded" alt="pic5">
+            <h4 class="pt-3 mb-3 fw-normal text-uppercase">Set Password</h4>
+            <p class="text-secondary">Set a password for disciplinary!</p>
+        </div>
+        <div class="col-md-4 col-sm-6 bg-light text-center py-5 px-3">
+            <i class="bi bi-book text-danger fs-1"></i>
+            <img src="assets/shaungram/pic6.png" class="rounded" alt="pic6">
+            <h4 class="pt-3 mb-3 fw-normal text-uppercase">Estimated your time</h4>
+            <p class="text-secondary">Set usage time in an app!</p>
+        </div>
+    </div>
+</div>
